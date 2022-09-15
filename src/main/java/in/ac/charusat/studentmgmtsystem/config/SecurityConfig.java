5e7016c337acc3f32ede1ba3037c275c9899cb47
dhaval20it162 <80237933+dhaval20it162@users.noreply.github.com>
2022-09-15 21:33:45 +0530

    B8 Added security with custom users in memory authentication

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -2,24 +2,18 @@ package in.ac.charusat.studentmgmtsystem.config;
 
 import net.bytebuddy.asm.Advice;
 import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    DataSource dataSource;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .withDefaultSchema()
+        auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("pass")
                 .roles("ADMIN", "USER")
